47140158c1ad91412ed032b8a9ea17fef8a1ab13
attimabbe98 <hosamaniattimabbe96@gmail.com>
2020-07-30 22:06:48 +0530

    Secont Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,11 @@ class HelloWorld
 {
 public static void main(String args[])
 {
+
 System.out.println("Hello world program");
+for(int i=0;i<5;i++)
+{
+System.out.println(i+"Hello world program");
+}
 }
 }
